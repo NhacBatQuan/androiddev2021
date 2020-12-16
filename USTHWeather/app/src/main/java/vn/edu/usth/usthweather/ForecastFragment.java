744d5a399123ab62b7174a2078961a74d21ae219
NhacBatQuan <59557905+NhacBatQuan@users.noreply.github.com>
2020-12-16 18:39:14 +0700

    upload practical 5

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -17,15 +17,36 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.WHITE);
+        view.setBackgroundColor(Color.parseColor("#5087CEEB"));
         LinearLayout liLay = view.findViewById(R.id.lilay);
         liLay.setOrientation(LinearLayout.VERTICAL);
-        TextView tevi = new TextView(getContext());
-        tevi.setText("Thursday");
-        ImageView imvi = new ImageView(getContext());
-        imvi.setImageResource(R.drawable.nuppercase_hat);
-        liLay.addView(tevi);
-        liLay.addView(imvi);
+        for (int i = 0; i < 7; i++) {
+            LinearLayout.LayoutParams layliparams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, 100);
+            layliparams.setMargins(40, 20, 40, 20);
+
+            LinearLayout.LayoutParams teviParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,2f);
+
+            LinearLayout.LayoutParams imviParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,2f);
+
+            LinearLayout.LayoutParams texviParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,1f);
+
+            LinearLayout layli = new LinearLayout(getContext());
+            layli.setOrientation(LinearLayout.HORIZONTAL);
+            liLay.addView(layli,layliparams);
+            TextView tevi = new TextView(getContext());
+            tevi.setText("Thursday");
+            TextView texvi = new TextView(getContext());
+            texvi.setText("Sunny\n35C");
+            ImageView imvi = new ImageView(getContext());
+            imvi.setImageResource(R.drawable.nuppercase_hat);
+            layli.addView(tevi,teviParams);
+            layli.addView(imvi,imviParams);
+            layli.addView(texvi,texviParams);
+        }
         return view;
     }
 }
